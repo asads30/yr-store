66b85ffdc5b9e46a33e3784e5123abc5ac1b24b9
asads30 <itmasteruz@yandex.com>
2023-03-28 18:51:51 +0500

    05

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -40,7 +40,7 @@
       </router-link>
     </div>
     <div class="footer">
-      <router-link to="/add-anons" class="footer__btn" v-if="data.name == null">Написать анонс</router-link>
+      <router-link to="/add-anons" class="footer__btn" v-if="anonse.name == null">Написать анонс</router-link>
       <router-link to="/edit-anons" class="footer__btn" v-else>Изменить анонс</router-link>
     </div>
   </q-page>
@@ -79,15 +79,10 @@ export default defineComponent({
   },
   setup() {
     const $store = useContentStore()
-    const getData = computed(() => {
-      return $store.state.data
-    })
-    const getCategories = computed(() => {
-      return $store.state.data
-    })
+    const { getData, getCategories } = $store
     return{
-      categories: (getCategories.length > 0) ? getCategories : null,
-      anonse: (getData.name == null) ? null : getData
+      categories: getCategories,
+      anonse: getData?.name
     }
   }
 })
