fe3901d7896261eee80b333f10c0d81cd12d5685
asads30 <itmasteruz@yandex.com>
2023-04-12 19:40:45 +0500

    0

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,7 +2,7 @@ import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/zapi',
+  baseURL: '/api',
   timeout: 5000,
   headers: {
     Authorization: localStorage.getItem('init_data')
